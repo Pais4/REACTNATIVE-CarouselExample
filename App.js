bfb587b9ddce68a090a9f44515749b85b03e60dd
Mateoslopera <mateoslopera@gmail.com>
2020-12-15 21:04:52 -0500

    Carousel con animacion

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Index } from './src/Screens/Index';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Index />
+    </>
+  );
+};
+
+export default App;
